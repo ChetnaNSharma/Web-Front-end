e34da912b36b062f7e220b3d18c269546fb7c909
ChetnaNSharma <chetna.ns.sharma@gmail.com>
2022-03-14 20:53:26 +0530

    Templating

--- Backend/EJS intro/index.js
+++ Backend/EJS intro/index.js
@@ -1,11 +1,34 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+const redditData = require('./data.json');
+
 
 app.set('view engine', 'ejs');
 
-app.get('/', (req,res) =>{
-    res.render('home');
+app.get('/', (req,res)=>{
+        res.render('home');
+});
 
+app.get('/cats', (req,res) => {
+    const myCats = ["fluffy", "richie", "pirahna", "goblin"];
+    res.render('Cats', {myCats});
+});
+
+app.get('/random', (req,res)=>{
+    const num  = Math.floor(Math.random() * 10) +1;
+    res.render('random', {rand : num});
+});
+
+app.get('/r/:subreddit',(req,res) =>{
+    const {subreddit} = req.params;
+    const data = redditData[subreddit];
+    if(data){
+    res.render('subreddit',{...data});
+    }
+    else{
+        res.render('not present');
+    }
 });
 
 app.listen(3000, () => {
